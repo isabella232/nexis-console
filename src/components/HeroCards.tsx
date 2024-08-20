af30be0270d438dc86adc0b2b6ddfb680f91a463
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-08-20 16:12:27 +0530

    hero cards

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -6,7 +6,7 @@ const HeroCards: React.FC = () => {
   return (
     <div className="overflow-x-hidden my-8">
       <div className="flex justify-center space-x-4">
-      <div className="relative flex-shrink-0 w-[300px] h-[300px] transition-transform duration-300 transform hover:scale-110 hover:animate-shake">
+        <div className="relative flex-shrink-0 w-[300px] h-[300px] transition-transform duration-300 transform hover:scale-110 hover:animate-shake cursor-pointer">
           <Image
             src="/cards/hero/build.png"
             alt="Build"
@@ -15,17 +15,16 @@ const HeroCards: React.FC = () => {
             priority
           />
         </div>
-        <div className="relative flex-shrink-0 w-[300px] h-[300px] transition-transform duration-300 transform hover:scale-110 hover:animate-shake">
+        <div className="relative flex-shrink-0 w-[300px] h-[300px] transition-transform duration-300 transform hover:scale-110 hover:animate-shake cursor-pointer">
           <Image
             src="/cards/hero/stake.png"
             alt="Stake"
-            layout="fill" // Makes the image fill the container
-            objectFit="contain" // Ensures aspect ratio is maintained
+            layout="fill"
+            objectFit="contain"
             priority
           />
         </div>
-      
-        <div className="relative flex-shrink-0 w-[300px] h-[300px] transition-transform duration-300 transform hover:scale-110 hover:animate-shake">
+        <div className="relative flex-shrink-0 w-[300px] h-[300px] transition-transform duration-300 transform hover:scale-110 hover:animate-shake cursor-pointer">
           <Image
             src="/cards/hero/bridge.png"
             alt="Bridge"
