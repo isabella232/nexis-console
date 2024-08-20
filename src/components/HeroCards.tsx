97e33426bbeda198b09e16ade3e8fe1168d5f595
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-08-20 15:44:46 +0530

    shake and zoom

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -4,32 +4,32 @@ import Image from 'next/image';
 
 const HeroCards: React.FC = () => {
   return (
-    <div className="overflow-x-hidden my-8">
+    <div className="overflow-x-hidden my-8 ">
       <div className="flex justify-center space-x-4">
-        <div className="flex-shrink-0">
+        <div className="relative flex-shrink-0 transform transition-transform duration-300 hover:scale-105 hover:animate-shake">
           <Image
             src="/cards/hero/stake.png"
             alt="Stake"
             width={300}
-            height={300} 
-            priority
-          />
-        </div>
-        <div className="flex-shrink-0">
-          <Image
-            src="/cards/hero/build.png"
-            alt="Build"
-            width={300} 
             height={300}
             priority
           />
         </div>
-        <div className="flex-shrink-0">
+        <div className="relative flex-shrink-0 transform transition-transform duration-300 hover:scale-105 hover:animate-shake">
+          <Image
+            src="/cards/hero/build.png"
+            alt="Build"
+            width={300}
+            height={300}
+            priority
+          />
+        </div>
+        <div className="relative flex-shrink-0 transform transition-transform duration-300 hover:scale-105 hover:animate-shake">
           <Image
             src="/cards/hero/bridge.png"
             alt="Bridge"
-            width={300} // Adjust width as needed
-            height={300} // Adjust height as needed
+            width={300}
+            height={300}
             priority
           />
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        shake: {
+          '0%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-5px)' },
+          '50%': { transform: 'translateX(5px)' },
+          '75%': { transform: 'translateX(-5px)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        shake: 'shake 0.5s ease-in-out',
+      },
     },
   },
   plugins: [],
