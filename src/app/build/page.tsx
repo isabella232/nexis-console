6945499fa4994350c330f8fb40a1cba8ff1958f3
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-08-24 19:54:04 +0530

    build page

---
+++ src/app/build/page.tsx
@@ -0,0 +1,14 @@
+// /src/app/build/page.js
+
+import React from 'react';
+
+const BuildPage = () => {
+  return (
+    <div>
+      <h1>Build Page</h1>
+      <p>This is the build page of the project.</p>
+    </div>
+  );
+};
+
+export default BuildPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import React from 'react'
 function Home() {
   return (
     <>
-    <Nav/>
     <HeroSection/>
     <HeroCards/>
     <DevTools/>
