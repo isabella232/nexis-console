81a7c76d280010bf5dac0d1c87b2e735cb75cf9b
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-08-25 12:25:38 +0530

    fixed bg

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const BuildPage = () => {
   return (
-    <div>
-      <img src="/bg/tl.png" className='absolute top-0 left-0' alt="" />
-      <img src="/bg/br.png" className='absolute right-0' alt="" />
-      <img src="/bg/stakeNzt.png" className='absolute' alt="" />
+    <> 
+      <img src="/bg/tl.png" className="absolute top-0 left-0 z-0  w-[80vw] h-[90vh]" alt="Top Left" />
+    <div className="relative min-h-screen">
+      <img src="/bg/br.png" className="absolute bottom-0 right-0 z-0 w-[80vw] h-[90vh]" alt="Bottom Right" />
+      <div className="relative z-10 flex items-center justify-center min-h-screen">
+        <h1 className="text-3xl font-bold text-center">Build Page</h1>
+        <p className="text-lg text-center mt-4">This is the build page of the project.</p>
+      </div>
     </div>
+    </>
   );
 };
 
