a8897b2e5a6372ba4495a6f35a67399d516effc7
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-08-25 12:34:35 +0530

    glassmorphic container

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -7,8 +7,10 @@ const BuildPage = () => {
     <div className="relative min-h-screen">
       <img src="/bg/br.png" className="absolute bottom-0 right-0 z-0 w-[80vw] h-[90vh]" alt="Bottom Right" />
       <div className="relative z-10 flex items-center justify-center min-h-screen">
-        <h1 className="text-3xl font-bold text-center">Build Page</h1>
-        <p className="text-lg text-center mt-4">This is the build page of the project.</p>
+      <img src="/bg/stakeNzt.png" className='absolute top-[10vh]' alt="" />
+      <div className='stake-container absolute top-[5vh] flex p-4'>
+        <div>Stake</div>
+      </div>
       </div>
     </div>
     </>
